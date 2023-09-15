21f1825ef8f1fa09ccd31a78822ef3d13673292c
daone <jcn07253@gmail.com>
2023-09-15 23:06:33 +0900

    스웨거 변경

--- src/main/java/com/example/catpus/global/config/SwaggerConfig.java
+++ src/main/java/com/example/catpus/global/config/SwaggerConfig.java
@@ -1,24 +1,26 @@
 package com.example.catpus.global.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Catpus",
+                description = "Catpus API 명세",
+                version = "v1")
+)
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
-    }
+    public GroupedOpenApi OpenApi() {
+        String[] paths = {"/**"};
 
-    private Info apiInfo() {
-        return new Info()
-                .title("80000 Coding API")
-                .description("80000 Coding API Docs")
-                .version("1.0.0");
+        return GroupedOpenApi.builder()
+                .group("Catpus API v1")
+                .pathsToMatch(paths)
+                .build();
     }
 }
