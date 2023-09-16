f182dd2eb88c1362586eece5e3eeb4d5a236f1b5
daone <jcn07253@gmail.com>
2023-09-16 14:31:18 +0900

    스웨거 ui 접속 안되는 문제 해결

--- src/main/java/com/example/catpus/domain/user/controller/SignController.java
+++ src/main/java/com/example/catpus/domain/user/controller/SignController.java
@@ -4,6 +4,8 @@ import com.example.catpus.domain.user.dto.SignRequest;
 import com.example.catpus.domain.user.dto.SignResponse;
 import com.example.catpus.domain.user.repository.UserRepository;
 import com.example.catpus.domain.user.service.SignService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,27 +13,32 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Sign", description = "회원가입 및 로그인")
 public class SignController {
 
     private final UserRepository userRepository;
     private final SignService userService;
 
     @PostMapping(value = "/login")
+    @Operation(summary ="로그인")
     public ResponseEntity<SignResponse> signin(@RequestBody SignRequest request) throws Exception {
         return new ResponseEntity<>(userService.login(request), HttpStatus.OK);
     }
 
     @PostMapping(value = "/register")
+    @Operation(summary ="회원가입")
     public ResponseEntity<Boolean> signup(@RequestBody SignRequest request) throws Exception {
         return new ResponseEntity<>(userService.register(request), HttpStatus.OK);
     }
 
     @GetMapping("/user/get")
+    @Operation(summary ="일반 회원 정보 ('/user/get?username=아이디' 로 조회 가능)")
     public ResponseEntity<SignResponse> getUser(@RequestParam String username) throws Exception {
         return new ResponseEntity<>( userService.getUser(username), HttpStatus.OK);
     }
 
     @GetMapping("/admin/get")
+    @Operation(summary ="관리자")
     public ResponseEntity<SignResponse> getUserForAdmin(@RequestParam String username) throws Exception {
         return new ResponseEntity<>( userService.getUser(username), HttpStatus.OK);
     }
--- src/main/java/com/example/catpus/domain/user/controller/UserController.java
+++ src/main/java/com/example/catpus/domain/user/controller/UserController.java
@@ -2,12 +2,10 @@ package com.example.catpus.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @Slf4j
-@RequestMapping("/api/user/manage")
 public class UserController {
 }
