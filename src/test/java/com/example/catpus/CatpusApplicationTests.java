8b7b9c87f148a18d988d13bf5a05f39d45dca368
daone <jcn07253@gmail.com>
2023-09-17 00:44:10 +0900

    fix

--- src/test/java/com/example/catpus/CatpusApplicationTests.java
+++ src/test/java/com/example/catpus/CatpusApplicationTests.java
@@ -1,13 +1,7 @@
 package com.example.catpus;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CatpusApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
